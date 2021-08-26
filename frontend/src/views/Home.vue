d4d202395a0196857aa22b42d7f64d5a81d683b0
Daniel Correa <yo@danielgara.com>
2021-08-26 13:39:21 -0500

    chapter 13

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div class="card">
+      <div class="card-header">
+        Movies
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Movies Application</h5>
+        <p class="card-text">Welcome to the Movies Application</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
