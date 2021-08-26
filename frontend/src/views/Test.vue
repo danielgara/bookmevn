85281d59dc187eb3e37344de6e518b4b0f64cb1f
Daniel Correa <yo@danielgara.com>
2021-08-26 14:18:44 -0500

    imp: chapter 14

---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- Header -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      ...
+    </nav>
+    <!-- Header -->
+    <div class="container mt-4">
+      <router-view/>
+    </div>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted mt-4">
+      <div class="text-center p-4" style="background-color: gainsboro;">
+        © 2021 Copyright -
+        <a class="text-reset fw-bold text-decoration-none" 
+          target="_blank" href="https://twitter.com/greglim81"
+        >
+          Greg Lim
+        </a> -
+        <a class="text-reset fw-bold text-decoration-none" 
+          target="_blank" href="https://twitter.com/danielgarax"
+        >
+          Daniel Correa
+        </a>
+      </div>
+    </footer>
+    <!-- Footer -->
+  </div>
+</template>
