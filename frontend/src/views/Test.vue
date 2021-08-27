0c6fff9140e478415162bfdef6c794b7b926ee54
Daniel Correa <yo@danielgara.com>
2021-08-27 11:57:25 -0500

    imp: chapter 16

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -12,12 +12,12 @@
     <footer class="text-center text-lg-start bg-light text-muted mt-4">
       <div class="text-center p-4" style="background-color: gainsboro;">
         © 2021 Copyright -
-        <a class="text-reset fw-bold text-decoration-none" 
+        <a class="text-reset fw-bold text-decoration-none"
           target="_blank" href="https://twitter.com/greglim81"
         >
           Greg Lim
         </a> -
-        <a class="text-reset fw-bold text-decoration-none" 
+        <a class="text-reset fw-bold text-decoration-none"
           target="_blank" href="https://twitter.com/danielgarax"
         >
           Daniel Correa
