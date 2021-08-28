1e64e592b895d45e84e630b4f5b8d5182dca522a
Daniel Correa <yo@danielgara.com>
2021-08-27 19:21:55 -0500

    imp: chapter 17

---
+++ frontend/src/services/ReviewService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class ReviewService {
+  static async createReview(data) {
+    const res = await axios.post('http://localhost:8000/api/v1/movies/review', data);
+    return res;
+  }
+
+  static async deleteReview(data) {
+    const res = await axios.delete('http://localhost:8000/api/v1/movies/review', { data });
+    return res;
+  }
+
+  static async updateReview(data) {
+    const res = await axios.put('http://localhost:8000/api/v1/movies/review', data);
+    return res;
+  }
+}
