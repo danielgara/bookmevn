96f1ce14ec810817ca842c08f7ce6de816f3f243
Daniel Correa <yo@danielgara.com>
2021-08-29 15:16:31 -0500

    imp: edit

--- frontend/src/views/Movie.vue
+++ frontend/src/views/Movie.vue
@@ -27,8 +27,16 @@
               <h6 class="card-subtitle mb-2 text-muted">
                 {{ getFormattedDate(review.date) }}
               </h6>
-              <p class="card-text">{{ review.review }}</p>
-              <a class="btn btn-primary me-3"> Edit </a>
+              <p v-if="!review.editing" class="card-text">{{ review.review }}</p>
+              <p v-if="review.editing" class="card-text">
+                <input v-model="newReviewMessage" type="text" class="form-control" />
+              </p>
+              <a v-if="verifyAuthorship(review.user_id)"
+                v-on:click="editReview(review)"
+                class="btn btn-primary me-3"
+              >
+                Edit
+              </a>
               <a class="btn btn-danger"> Delete </a>
             </li>
           </ul>
@@ -42,6 +50,7 @@
 import * as moment from 'moment';
 import MovieService from '../services/MovieService';
 import AddReview from '../components/AddReview.vue';
+import ReviewService from '../services/ReviewService';
 
 export default {
   name: 'Movies',
@@ -58,6 +67,7 @@ export default {
         _id: '',
         reviews: [],
       },
+      newReviewMessage: '',
     };
   },
   created() {
@@ -68,10 +78,38 @@ export default {
       const movieData = await MovieService.getMovie(
         this.$route.params.id,
       );
+      const modifiedReviews = movieData.reviews.map((v) => ({ ...v, editing: false }));
+      movieData.reviews = modifiedReviews;
       this.movie = movieData;
     },
     getFormattedDate(date) {
       return moment(date).format('Do MMMM YYYY');
+    },
+    verifyAuthorship(reviewUserId) {
+      if (this.$store.state.user.id && this.$store.state.user.id === reviewUserId) {
+        return true;
+      }
+      return false;
+    },
+    editReview(review) {
+      if (review.editing) {
+        review.review = this.newReviewMessage;
+        this.saveUpdatedReview(review);
+        review.editing = false;
+      } else {
+        this.newReviewMessage = review.review;
+        review.editing = true;
+      }
+    },
+    async saveUpdatedReview(newReview) {
+      const data = {
+        review: newReview.review,
+        name: newReview.name,
+        user_id: newReview.user_id,
+        movie_id: newReview.movie_id,
+        review_id: newReview._id,
+      };
+      await ReviewService.updateReview(data);
     },
   },
 };
